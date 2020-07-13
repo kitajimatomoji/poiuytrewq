8fce609340f603dcfd71e4f63c4d564c96f77366
T.Kitajima <tomoji.kitajima@i-studio.co.jp>
2020-07-13 13:31:05 +0900

    test

--- logtest.py
+++ logtest.py
@@ -1,23 +1,13 @@
-import os
-import sys
-import random
-
 from cloudwatch_logger import CloudWatch_logger
 
-try:
-    ret = {"trigger":"edit_content"}
-    #ret = {"trigger":"application_update"}
-    ret["contentful_response_time"] = random.uniform(2, 10)
-    ret["contentful_data_amount"] = random.uniform(100000, 120000)
-    ret["content_building_time"] = random.uniform(5, 20)
+ret = {"trigger":"edit_content"}
+#ret = {"trigger":"application_update"}
+ret["contentful_response_time"] = 123
+ret["contentful_data_amount"] = 1230
+ret["content_building_time"] = 12
 
-    #str(uuid.uuid4())
+log_group_name = '/aws/lambda/logging_sample_daily'
+log_stream_name = '2020_06_25'
 
-    print(ret)
-
-    log_group_name = '/aws/lambda/logging_sample_daily'
-    log_stream_name = datetime.datetime.now().strftime('%Y_%m_%d')
-    #log_stream_name = '2020_06_29'
-
-    logger = CloudWatch_logger(log_group_name, log_stream_name)
-    logger.log(ret)
+logger = CloudWatch_logger(log_group_name, log_stream_name)
+logger.log(ret)
