7a06135ed82a07086df9d6d823a7e66c7a35b74b
T.Kitajima <tomoji.kitajima@i-studio.co.jp>
2020-07-13 14:08:59 +0900

    test

--- logtest.py
+++ logtest.py
@@ -1,14 +1,18 @@
 import cloudwatch_logger
 from cloudwatch_logger import CloudWatch_logger
 
-ret = {"trigger":"edit_content"}
-#ret = {"trigger":"application_update"}
-ret["contentful_response_time"] = 123
-ret["contentful_data_amount"] = 1230
-ret["content_building_time"] = 12
+try:
+    ret = {"trigger":"edit_content"}
+    #ret = {"trigger":"application_update"}
+    ret["contentful_response_time"] = 123
+    ret["contentful_data_amount"] = 1230
+    ret["content_building_time"] = 12
 
-log_group_name = '/aws/lambda/logging_sample_daily'
-log_stream_name = '2020_06_25'
+    log_group_name = '/aws/lambda/logging_sample_daily'
+    log_stream_name = '2020_06_25'
 
-logger = CloudWatch_logger(log_group_name, log_stream_name)
-logger.log(ret)
+    logger = CloudWatch_logger(log_group_name, log_stream_name)
+    logger.log(ret)
+
+except Exception as e:
+    print(e)
